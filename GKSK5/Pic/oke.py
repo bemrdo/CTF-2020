37ef8b31232c3f20b9e3f748d4af170fa8bb97e1
bemrdo <bemr.do@gmail.com>
2020-03-04 09:59:12 +0800

    GKSK#5

--- GKSK5/Pic/oke.py
+++ GKSK5/Pic/oke.py
@@ -1,10 +1,12 @@
 c = open('gambar2.png','rb').read()
-c = c.replace("\xaa\xaa\xaa\xaa", "1").replace("\xff\xff\xff\xff", "0")
+c = c.replace("\xaa\xaa\xaa\xaa", "0").replace("\xff\xff\xff\xff", "1")
 b = ''
 flag = ''
 
 for i in c:
-    if i in '01':
+    if i in '1':
+        b += i
+    elif i in '0':
         b += i
 
 for x in range(0, len(b), 8):
--- GKSK5/Rasa Sakit/solver.py
+++ GKSK5/Rasa Sakit/solver.py
@@ -1,36 +1,27 @@
 from sympy import *
 from Crypto.Util.number import *
 
-n = 10817338847826400693993693310389738162087305543112487544514182469845530740105439717227
-e = 13337
-c = 1712742277417960630321571765396625971360509211869464293909518739909852740164342992459
+def getDec(n, e, c):
+    p, q = factorint(n)
+    phi = (p-1) * (q-1)
+    d = inverse(e,phi)
+    dec = pow(c, d, n)
+    return dec
 
-from decimal import *
-import math
-import re
+def getFlag(decs):
+    flag = ''
+    decs = decs.split('-')
+    for x in decs:
+        flag += chr(int(x))
+    return flag
 
-getcontext().prec = 100
+def main():
+    n = 10817338847826400693993693310389738162087305543112487544514182469845530740105439717227
+    e = 13337
+    c = 1712742277417960630321571765396625971360509211869464293909518739909852740164342992459
+    print getDec(n, e, c)
+    decs = '71-75-83-75-123-84-52-107-95-98-49-53-52-95-109-51-110-52-104-52-110-95-82-83-52-95-53-52-107-49-84-125'
+    print getFlag(decs)
 
-def numtoASCII(number):
-    parsing = hex(int(number))
-    parsing = re.sub("0x", "", str(parsing))
-    parsing = re.sub("L", "", str(parsing))
-    start = 0
-    end = 2
-    decode = ""
-    while end <= len(parsing):
-        hexForm = "0x" + str(parsing[start:end])
-        decode += chr(int(hexForm, 16))
-        start += 2
-        end += 2
-    return decode
-
-
-p, q = factorint(n)
-print p
-print q
-phi = (p-1) * (q-1)
-d = inverse(e,phi)
-dec = pow(c, d, n)
-print numtoASCII(dec)
-print long_to_bytes(dec)
+if __name__ == '__main__':
+    main()
